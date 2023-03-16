a86d9df82a46c4971d322dded3bbdc947213df57
mininim <minsoplee2015@gmail.com>
2023-03-16 20:46:25 +0900

    Initial Commit

---
+++ HakSikThat/HakSikThatApp.swift
@@ -0,0 +1,17 @@
+//
+//  HakSikThatApp.swift
+//  HakSikThat
+//
+//  Created by Eric Lee on 2023/03/16.
+//
+
+import SwiftUI
+
+@main
+struct HakSikThatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
